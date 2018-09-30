646f402153edd65416eff8109dd085665f317316
Yadvaindera Sood <ydsood@gmail.com>
2018-09-30 19:36:37 -0400

    Fixes #1. Adding a sample export to test that examples can consume an export from the module

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'redux-form-datagrid';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color='red'>
+          Like
+        </Button>
       </div>
     );
   }

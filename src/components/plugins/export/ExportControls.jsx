4c71924b9a4f35d238ded057ff1674622a0e3603
yli <yli@fasttechnology.com>
2024-10-25 09:18:07 -0400

    fix local export control issue

--- src/components/plugins/export/ExportControls.jsx
+++ src/components/plugins/export/ExportControls.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "semantic-ui-react";
 import exportUtil from "./util";
 import type {
@@ -12,30 +12,33 @@ type Props = {
   exportFileName?: string,
 }
 
-class ExportControls extends Component<Props> {
-  constructor(props: Props) {
-    super(props);
-    this.exportData = this.exportData.bind(this);
-    this.data = props.store.getData();
-  }
+const ExportControls = (props: Props) => {
+  const [data, setData] = useState([]);
+  const {
+    store, columnModel, exportFileName, exportButtonLabel,
+  } = props;
+  useEffect(() => {
+    const gridData = store.getData();
+    setData(gridData);
+  }, []);
 
-  exportData() {
-    const { exportFileName, columnModel } = this.props;
-    exportUtil(exportFileName, this.data, columnModel);
-  }
+  useEffect(() => {
+    const gridData = store.getData();
+    setData(gridData);
+  }, [store]);
 
-  render() {
-    const { exportButtonLabel } = this.props;
-    if (this.data.length) {
-      return (
-        <Button.Group basic compact>
-          <Button icon="file excel" content={exportButtonLabel} onClick={() => this.exportData()} />
-        </Button.Group>
-      );
-    }
-    return "";
+  const exportData = () => {
+    exportUtil(exportFileName, data, columnModel);
+  };
+  if (data?.length) {
+    return (
+      <Button.Group basic compact>
+        <Button icon="file excel" content={exportButtonLabel} onClick={() => exportData()} />
+      </Button.Group>
+    );
   }
-}
+  return "";
+};
 
 ExportControls.defaultProps = {
   exportButtonLabel: "Export",
